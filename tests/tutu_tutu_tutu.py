74f9c91cab1045bfc663ce5223f96be464f828cd
maratkanov-a <a.s.maratkanov@gmail.com>
2016-11-27 20:19:53 +0300

    refactoring

--- tests/tutu_tutu_tutu.py
+++ tests/tutu_tutu_tutu.py
@@ -1,42 +1,48 @@
+import socket
 import unittest
 
 import re
 
+import requests
 from mock import Mock
+from seismograph.ext.selenium.polling import do, wrap
 from selenium.webdriver.remote.webelement import WebElement
 
-from seismograph.ext.selenium.proxy.proxy import BaseProxy, WebElementProxy
+from seismograph.ext.selenium.proxy.proxy import WebElementProxy
 
 from seismograph.ext.selenium.utils import change_name_from_python_to_html
 
-from seismograph.ext.selenium import drivers
 from seismograph.ext.selenium.router import Router
 
 from seismograph.ext.selenium import add_route
-from seismograph.ext.selenium.browser import BrowserConfig
 from seismograph.ext.selenium.exceptions import RouteNotFound, RouterError
-from seismograph.ext.selenium.proxy import WebDriverProxy
-from tests.lib.case import BaseTestCase, ConfigTestCaseMixin
+from tests.lib.case import BaseTestCase
 from seismograph.ext.selenium.query import QueryObject, ValueFormat, ATTRIBUTE_ALIASES, attribute_by_alias, Expression, \
-    Equal, ContainsText, TAG_ALIASES, tag_by_alias, QueryResult
+    ContainsText, TAG_ALIASES, tag_by_alias, QueryResult
 
 
-#
-# class PollingTests(unittest.TestCase):
-#     # class PollingTests(BaseTestCase):
-#     def test_la(self):
-#         # def runTest(self):
-#         from seismograph.ext.selenium.polling import HTTPException as H
-#         H = None
-#         self.assertRaises(ImportError, H)
-#         # def test_wrapper(self):
-#
+class PollingTests(unittest.TestCase):
+    def test_import(self):
+        from seismograph.ext.selenium.polling import HTTPException as H
+        H = None
+        self.assertRaises(ImportError, H)
 
-class RouterTests(BaseTestCase):
-# class RouterTests(unittest.TestCase):
+    def a(self):
+        return self
+
+    def test_do(self):
+        self.assertNotEqual(wrap()(self.a), self.a)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind(('', 8000))
+
+        do(requests.get)()()
+
+
+class RouterTests(unittest.TestCase):
     @classmethod
     def a_func(cls, a):
         return a
+
     def test_proxy(self):
         router = Router(123)
         router.__proxy = 123
@@ -44,8 +50,10 @@ class RouterTests(BaseTestCase):
 
     def test_rule(self):
         self.assertDictEqual(Router.__rules__, {})
+
         Router.add_rule(123, '')
         rule = re.compile(r'^{}$'.format(123))
+
         self.assertEqual(Router.__rules__[rule], '')
         Router.__rules__ = {}
 
@@ -72,17 +80,18 @@ class RouterTests(BaseTestCase):
         self.assertEqual(res, mock_file)
         Router.__rules__ = {}
 
-    def test_get_2(self):
+    def test_get_except(self):
         mock_file = Mock()
         mock_file.config.PROJECT_URL = None
         mock_file.browser = {"FAKE": "FAKE"}
+
         try:
             Router(mock_file).get('1')
         except RouteNotFound as route_error:
             self.assertEqual(route_error.message, '1')
         Router.__rules__ = {}
 
-    def test_get_3(self):
+    def test_get_without_config(self):
         mock_file = Mock()
         mock_file.config.PROJECT_URL = None
         mock_file.browser = {"FAKE": "FAKE"}
@@ -111,17 +120,20 @@ class RouterTests(BaseTestCase):
         Router.__rules__ = {}
 
 
-# class QueryTests(BaseTestCase):
 class QueryTests(unittest.TestCase):
-    def test_QueryObject_methods(self):
+    def test_QueryObject_init_method(self):
         query = QueryObject('la', el={"la": 123})
         self.assertEqual(query.tag, 'la')
         self.assertEqual(query.selector, {"el": {"la": 123}})
 
+    def test_QueryObject_str_method(self):
+        query = QueryObject('la', el={"la": 123})
         res = query.__str__()
         waited_answer = u'<{} {}>'.format(query.tag, u' '.join((u'{}="{}"'.format(k, v) for k, v in query.selector.items())),)
         self.assertEqual(res, waited_answer)
 
+    def test_QueryObject_call_method(self):
+        query = QueryObject('la', el={"la": 123})
         proxy = WebElementProxy(WebElement('1', 1))
         query.__call__(proxy)
         self.assertIsInstance(query, QueryObject)
@@ -147,44 +159,43 @@ class QueryTests(unittest.TestCase):
         )
         self.assertEqual(res, attribute_by_alias('one'))
 
-    def test_make_result(self):
-        pass
-
-    def test_execute(self):
-        pass
-
     def test_ValueFormat_methods(self):
         value = ValueFormat('id')
 
         try:
             value.__call__()
-        except ValueError as vaue_error:
-            self.assertEqual(vaue_error.message, 'Unknown format')
+        except ValueError as value_error:
+            self.assertEqual(value_error.message, 'Unknown format')
 
         value.__format__ = '{attr_value} {attr_name}'
         res = value.__call__('id')
         self.assertEqual(res, '{} {}'.format(value.value, attribute_by_alias('id')))
 
-    def test_Expression_metods(self):
+    def test_Expression_init_method(self):
         expression = Expression()
         self.assertListEqual(expression._expressions, [])
 
+    def test_Expression_expression_method(self):
         expression = Expression(12)
         self.assertEqual(expression.expression(12), expression)
 
+    def test_Expression_with_ValueFormat(self):
         value_format = ValueFormat('id')
         value_format.__format__ = '{attr_value} {attr_name}'
 
         expression = Expression(value_format)
         self.assertEqual(expression.expression(value_format), expression)
 
+    def test_Expression_with_ContainsText_exept(self):
         try:
             expression = Expression(ContainsText(123))
         except ValueError as error:
             self.assertEqual(error.message, 'Restricted format "ContainsText"')
 
+    def test_Expression_call_method(self):
+        expression = Expression(12)
         res = expression.__call__('nice')
-        self.assertEqual(res, 'id niceid nice')
+        self.assertEqual(res, '[nice="12"]')
 
     def test_QueryResult_methods(self):
         mock = Mock()
@@ -206,9 +217,6 @@ class QueryTests(unittest.TestCase):
 
         res = query.wait()
         self.assertNotEqual(res, mock)
-
-
-
 
 if __name__ == '__main__':
     unittest.main()
